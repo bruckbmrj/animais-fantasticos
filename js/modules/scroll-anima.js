f342f0b961eb2af79fc165ac77236bf52a2fd765
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2022-02-26 15:52:08 -0300

    animação ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,8 +3,31 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
-    console.log('anima scroll');
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  /* pega a distância de
+  cada item em relação ao site */
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
+      }
+    });
   }
 
   animaScroll() {
@@ -20,7 +43,16 @@ export default class ScrollAnima {
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // remove o evento de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
